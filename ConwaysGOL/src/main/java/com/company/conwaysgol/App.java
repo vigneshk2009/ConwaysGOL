56e237454da3abb89af70b2bb82a2a155f0f4166
mohanethra <mohanethra@mohanethras-MacBook-Pro.local>
2018-10-31 17:08:11 +1100

    First Commit

---
+++ ConwaysGOL/src/main/java/com/company/conwaysgol/App.java
@@ -0,0 +1,13 @@
+package com.company.conwaysgol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
